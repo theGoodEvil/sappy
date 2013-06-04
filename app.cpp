631ea15c6ddf62001cec19956f5bb30586e40f05
Guido Lorenz <code@superquadratic.net>
2013-06-04 16:21:56 +0200

    Add first sappy version

---
+++ app.cpp
@@ -0,0 +1,87 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2013 the Good Evil GmbH
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+////////////////////////////////////////////////////////////////////////////////
+
+#include "app.h"
+
+using namespace Sifteo;
+
+namespace sappy {
+
+App* App::instance = 0;
+
+App::App() {
+  ASSERT(instance == 0 && "You can only create one App instance");
+  instance = this;
+
+  Events::cubeConnect.set(&App::onCubeConnect, this);
+  Events::cubeDisconnect.set(&App::onCubeDisconnect, this);
+  Events::cubeRefresh.set(&App::onCubeRefresh, this);
+  Events::cubeTouch.set(&App::onCubeTouch, this);
+}
+
+App* App::getInstance() {
+  return instance;
+}
+
+void App::run() {
+  for(CubeID cid : CubeSet::connected()) {
+    onCubeConnect(cid);
+  }
+  
+  for (;;) {
+    System::paint();
+
+    timeStep.next();
+    onLoop(timeStep.delta());
+
+    for(Cube& cube : cubes) {
+      cube.update();
+    }
+  }
+}
+
+Cube& App::getCube(unsigned cid) {
+  ASSERT(cid < CUBE_ALLOCATION);
+  return cubes[cid];
+}
+
+void App::onCubeConnect(unsigned cid) {
+  Cube& cube = cubes[cid];
+  cube.connect(cid);
+}
+
+void App::onCubeDisconnect(unsigned cid) {
+  Cube& cube = cubes[cid];
+  cube.disconnect();
+}
+
+void App::onCubeRefresh(unsigned cid) {
+  Cube& cube = cubes[cid];
+  cube.refresh();
+}
+
+void App::onCubeTouch(unsigned cid) {
+  Cube& cube = cubes[cid];
+  cube.touch();
+}
+
+}
